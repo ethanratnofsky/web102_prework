befff6a797ed4e84a5396f7afa31110b0f18c29c
Ethan Ratnofsky <43038541+ethanratnofsky@users.noreply.github.com>
2022-12-21 11:10:52 -0500

    Add game cards

--- index.js
+++ index.js
@@ -29,27 +29,50 @@ const gamesContainer = document.getElementById("games-container");
 function addGamesToPage(games) {
 
     // loop over each item in the data
-
+    for (let i = 0; i < games.length; i++) {
 
         // create a new div element, which will become the game card
-
+        const gameCard = document.createElement("div");
 
         // add the class game-card to the list
-
+        gameCard.classList.add("game-card");
 
         // set the inner HTML using a template literal to display some info 
         // about each game
         // TIP: if your images are not displaying, make sure there is space
         // between the end of the src attribute and the end of the tag ("/>")
-
+        const gameCardContents = `
+            <img class="game-img" src="${games[i].img}" alt="${games[i].name}" />
+            <h4 class="game-name">${games[i].name}</h4>
+            <div class="game-description">${games[i].description}</div>
+            <br />
+            <div class="game-stats">
+                <div class="game-pledged">
+                    <div class="game-pledged-label">Pledged</div>
+                    <div class="game-pledged-value">$${games[i].pledged.toLocaleString()}</div>
+                </div>
+                <br />
+                <div class="game-goal">
+                    <div class="game-goal-label">Goal</div>
+                    <div class="game-goal-value">$${games[i].goal.toLocaleString()}</div>
+                </div>
+                <br />
+                <div class="game-backers">
+                    <div class="game-backers-label">Backers</div>
+                    <div class="game-backers-value">${games[i].backers.toLocaleString()}</div>
+                </div>
+            </div>
+        `;
+        gameCard.innerHTML = gameCardContents;
 
         // append the game to the games-container
-
+        gamesContainer.appendChild(gameCard);
+    }
 }
 
 // call the function we just defined using the correct variable
 // later, we'll call this function using a different list of games
-
+addGamesToPage(GAMES_JSON);
 
 /*************************************************************************************
  * Challenge 4: Create the summary statistics at the top of the page displaying the
